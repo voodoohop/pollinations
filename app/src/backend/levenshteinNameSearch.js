95611853a5215c67d6b9f0410ff956bae90e67b3
Thomas Haferlach <t.haferlach@gmail.com>
2021-09-10 20:45:17 +0200

    Shuffle names

--- app/src/backend/levenshteinNameSearch.js
+++ app/src/backend/levenshteinNameSearch.js
@@ -4,6 +4,10 @@ import r from "ramda";
 import names from "../data/names.js";
 
 
+const shuffle = array => array.sort(() => 0.5 - Math.random());
+
 const notebookName = process.argv[2];
+
+shuffle(names);
 
 console.log(r.sortBy(name => levenshtein.get(notebookName,name), names)[0]);
