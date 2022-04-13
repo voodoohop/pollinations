b7df1ea263aee6e1d366c8bfdb6ab6dbd2f1cdae
ale-rls <85608868+ale-rls@users.noreply.github.com>
2022-04-13 10:33:26 +0200

    footer more info component

---
+++ app/src/components/organisms/More.js
@@ -0,0 +1,10 @@
+import Link from '@material-ui/core/Link'
+
+
+const More = () => <div style={{ margin: '1em auto 4em auto' }}>
+    Discuss, get help and contribute on
+    <Link href="https://github.com/pollinations/pollinations" target="_blank"> [ Github ] </Link>
+    or <Link href="https://discord.gg/XXd99CrkCr" target="_blank">[ Discord ]</Link>.
+</div>
+
+export default More
