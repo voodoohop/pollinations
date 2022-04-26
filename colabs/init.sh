7bf05f4cbbf195560185321f739087ca11513f6a
Niels Warncke <niels.warncke@gmail.com>
2022-04-26 14:42:07 +0200

    single init script to run inside the colab container

--- colabs/init.sh
+++ colabs/init.sh
@@ -1,5 +1,16 @@
+echo "APT::Get::force-yes \"true\";"  >> /etc/apt/apt.conf.d/90forceyes
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 16
+nvm use 16
+
 wget https://github.com/pollinations/pollinations/archive/refs/heads/gcloud-gpu.zip
 unzip gcloud-gpu.zip
 mv pollinations-gcloud-gpu pollinations
+cd pollinations/app && npm run install_backend
+cd ~
 pip install papermill
-papermill pollinations/colabs/pollinator.ipynb pollinations/colabs/out.ipynb -p on_colab=False
+papermill pollinations/colabs/pollinator.ipynb pollinations/colabs/out.ipynb -p on_colab False -p node_id 123
